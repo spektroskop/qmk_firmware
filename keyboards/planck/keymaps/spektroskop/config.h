e10905cd8c3bc47396e362b0be689e4977c2bd1c
Alexander Stensrud <alex@yuki.no>
2020-04-10 12:10:01 +0200

    Added layout

---
+++ keyboards/planck/keymaps/spektroskop/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PLANCK_SOUND)
+#endif
+
+#define ENCODER_RESOLUTION 4
+
+#define UNICODE_SELECTED_MODES UC_LNX, UC_OSX
+
+// #define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 300
+
+#define TAPPING_TERM 150
